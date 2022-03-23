5e45ee861081392b97022daec57a4dbed725794b
Jet Kan <jetkan.yk@gmail.com>
2022-03-23 20:31:29 +0800

    Implement basic clustering

--- phase2/emoji_clustering.py
+++ phase2/emoji_clustering.py
@@ -3,8 +3,9 @@ import torch
 from sklearn.cluster import KMeans
 from sklearn.decomposition import PCA
 
-from feature_extraction import SAVE_FILEPATH
+from feature_extraction import DEVICE, SAVE_FILEPATH
 
+N_CLUSTERS = 5
 RAND_SEED = 27
 
 
@@ -12,28 +13,34 @@ def main():
     """
     Load feature embeddings from SAVE_FILEPATH
     """
-    dataset = torch.load(SAVE_FILEPATH)
+    dataset = torch.load(SAVE_FILEPATH, DEVICE)
+
     features = []
     num_em = 0
     for data in dataset:
         for embedding in data["embeddings"]:
             features.append(embedding)
         num_em += data["num_emoji"] + 1
+    features = torch.stack(features)
 
     assert len(features) == num_em
     assert len(features[0]) == 768
 
+    print(f"Loaded features: {SAVE_FILEPATH} as a Tensor: {features.shape}")
+
     """
     Fit features into KMeans model for clustering
     """
-    kmeans = KMeans(n_clusters=5, random_state=RAND_SEED)
+    kmeans = KMeans(n_clusters=N_CLUSTERS, random_state=RAND_SEED)
     kmeans.fit(features)
+
+    print(f"Fitted features into KMeans model: {kmeans}")
 
     """
     Visualization
     """
-    pca = PCA(n_components=2, random_state=RAND_SEED)
-    reduced_features = pca.fit_transform(features.toarray())
+    pca = PCA(random_state=RAND_SEED)
+    reduced_features = pca.fit_transform(features)
     reduced_cluster_centers = pca.transform(kmeans.cluster_centers_)
 
     plt.scatter(
@@ -46,7 +53,7 @@ def main():
         s=150,
         c="b",
     )
-    
+    plt.show()
 
 
 if __name__ == "__main__":
