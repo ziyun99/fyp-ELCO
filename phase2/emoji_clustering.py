d62c508b1cbc855b8eaee5f9a6898fd91c616e68
ziyun99 <e0313671@u.nus.edu>
2022-03-25 07:34:49 +0000

    Add elbow method

--- phase2/emoji_clustering.py
+++ phase2/emoji_clustering.py
@@ -10,11 +10,11 @@ from feature_extraction import DEVICE, EXPERIMENT_FOLDER, FEATURES_FILEPATH
 RAND_SEED = 27
 
 
-def load_features(filepath, device):
+def load_features(filepath):
     """
-    Load feature embeddings from SAVE_FILEPATH
+    Load feature embeddings from filepath into device.
     """
-    dataset = torch.load(filepath, device)
+    dataset = torch.load(filepath, DEVICE)
 
     features = []
     num_em = 0
@@ -31,19 +31,18 @@ def load_features(filepath, device):
     return features
 
 
-def cluster_features(features, n_clusters=6, random_state=RAND_SEED):
+def cluster_features(features, n_clusters=6):
     """
-    Fit features into KMeans model for clustering
+    Cluster features with KMeans model, generate a jpg and returns the
+    KMeans inertia value.
     """
-    kmeans = KMeans(n_clusters=n_clusters, random_state=random_state)
+    kmeans = KMeans(n_clusters=n_clusters, random_state=RAND_SEED)
     kmeans.fit(features)
 
     print(f"Fitted features into KMeans model: {kmeans}")
 
-    """
-    Visualization
-    """
-    pca = PCA(random_state=random_state)
+    # Visualization
+    pca = PCA(random_state=RAND_SEED)
     reduced_features = pca.fit_transform(features)
     reduced_cluster_centers = pca.transform(kmeans.cluster_centers_)
 
@@ -61,12 +60,31 @@ def cluster_features(features, n_clusters=6, random_state=RAND_SEED):
     PLOT_FILEPATH = os.path.join(EXPERIMENT_FOLDER, f"cluster-{n_clusters}.jpg")
     plt.savefig(PLOT_FILEPATH)
 
+    return kmeans.inertia_
+
+
+def plot_inertia(inertias, N):
+    """
+    Plot the n_clusters-to-inertia graph for elbow method
+    """
+    plt.plot(N, inertias, "bx-")
+    plt.xlabel("Clusters")
+    plt.ylabel("Sum_of_squared_distances")
+    plt.title("Elbow Method For Optimal k")
+
+    ELBOW_FILEPATH = os.path.join(EXPERIMENT_FOLDER, "elbow-method.jpg")
+    plt.savefig(ELBOW_FILEPATH)
+
 
 def main():
     features = load_features(FEATURES_FILEPATH, DEVICE)
 
-    for i in range(2, 11):
-        cluster_features(features, i)
+    inertias = []
+    N = range(2, 11)
+    for n in N:
+        inertias.append(cluster_features(features, n))
+
+    plot_inertia(inertias, N)
 
 
 if __name__ == "__main__":
